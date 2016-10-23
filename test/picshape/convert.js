83150790d36fb7fe211304ac35466a8c5fa8149b
Guillaume <guillaume.prier@ecole.ensicaen.fr>
2016-10-23 18:21:21 +0200

    Removed comments

--- test/picshape/convert.js
+++ test/picshape/convert.js
@@ -13,41 +13,41 @@ chai.use(chaiHttp);
 /*
   * Test the /GET route
   */
-// describe('/GET picshape/convert', () => {
-//     it('it should fail on GET', (done) => {
-//       chai.request(server)
-//           .get('/api/picshape/convert')
-//           .end((err, res) => {
-//               res.should.have.status(404);
-//             done();
-//           });
-//     });
-// });
-//
-// /*
-//   * Test the /POST route
-//   */
-// describe('/POST picshape/convert', function(){
-//     this.timeout(60000);
-//
-//     it('it should fail when not providing image', (done) => {
-//       chai.request(server)
-//           .post('/api/picshape/convert')
-//           .end((err, res) => {
-//               res.should.have.status(400);
-//             done();
-//           });
-//     });
-//
-//     it('it should success when providing image. Timeout - 60s', (done) => {
-//       setTimeout(done, 60000);
-//
-//       chai.request(server)
-//           .post('/api/picshape/convert')
-//           .attach('photo', fs.readFileSync(__dirname + '/test_patrick.jpg'), 'test_patrick.jpg')
-//           .end((err, res) => {
-//               res.should.have.status(200);
-//             done();
-//           });
-//     });
-// });
+describe('/GET picshape/convert', () => {
+    it('it should fail on GET', (done) => {
+      chai.request(server)
+          .get('/api/picshape/convert')
+          .end((err, res) => {
+              res.should.have.status(404);
+            done();
+          });
+    });
+});
+
+/*
+  * Test the /POST route
+  */
+describe('/POST picshape/convert', function(){
+    this.timeout(60000);
+
+    it('it should fail when not providing image', (done) => {
+      chai.request(server)
+          .post('/api/picshape/convert')
+          .end((err, res) => {
+              res.should.have.status(400);
+            done();
+          });
+    });
+
+    it('it should success when providing image. Timeout - 60s', (done) => {
+      setTimeout(done, 60000);
+
+      chai.request(server)
+          .post('/api/picshape/convert')
+          .attach('photo', fs.readFileSync(__dirname + '/test_patrick.jpg'), 'test_patrick.jpg')
+          .end((err, res) => {
+              res.should.have.status(200);
+            done();
+          });
+    });
+});
