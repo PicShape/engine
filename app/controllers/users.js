62185f79a340e98baf1c0bad7c92643bf5ed3c0d
benjamindebotte <benjamin.debotte@gmail.com>
2017-03-09 09:00:48 +0100

    Code quality improvment

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,22 +3,34 @@ var User = require('../models/User');
 
 exports.getUsers = function(req, res) {
     User.find({}, {id: 1, name: 1, email: 1}, function(err, users) {
+        if (err) {
+            res.status(400).send( { errorMessage: 'There have been errors.', errors: err });
+            return;
+        }
         res.send({users: users});
-    })
+    });
 }
 
 exports.getUserByName = function(req, res) {
     var name = req.params.name;
 
     User.findOne({name: new RegExp('^'+name+'$', "i")}, function(err, user) {
+        if (err) {
+            res.status(400).send( { errorMessage: 'There have been errors.', errors: err });
+            return;
+        }
         res.send({user: user});
-    })
+    });
 }
 
 exports.getUserById = function(req, res) {
     var id = req.params.id;
 
     User.findById(id, function(err, user) {
+        if (err) {
+            res.status(400).send( { errorMessage: 'There have been errors.', errors: err });
+            return;
+        }
         res.send({user: user});
-    })
+    });
 }
