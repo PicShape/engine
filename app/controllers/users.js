6656269c893f7b9a6059fe77a7ad7d47dbbfd3cd
benjamindebotte <benjamin.debotte@gmail.com>
2017-03-09 09:32:59 +0100

    Allez travis soit cool stp

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -9,28 +9,28 @@ exports.getUsers = function(req, res) {
         }
         res.send({users: users});
     });
-}
+};
 
 exports.getUserByName = function(req, res) {
     var name = req.params.name;
 
     User.findOne({name: new RegExp('^'+name+'$', "i")}, function(err, user) {
         if (err) {
-            res.status(400).send( { errorMessage: 'There have been errors.', errors: err });
+            res.status(400).send( { errorMessage: 'Error gathering users by name.', errors: err });
             return;
         }
         res.send({user: user});
     });
-}
+};
 
 exports.getUserById = function(req, res) {
     var id = req.params.id;
 
     User.findById(id, function(err, user) {
         if (err) {
-            res.status(400).send( { errorMessage: 'There have been errors.', errors: err });
+            res.status(400).send( { errorMessage: 'Error gathering users by id.', errors: err });
             return;
         }
         res.send({user: user});
     });
-}
+};
